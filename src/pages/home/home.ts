3056b5fe0d11b8059865c7c0e592028a4a2d72ab
wiselh <wise.self.2@gmail.com>
2018-04-18 12:17:55 +0100

    Login With Facebook
    -1 Create an app in developer.facebook.com
    -2 Enable facebook login in firebase and set up ID and secret key + copy and past "OAuth redirect URI" from Firebase to PRODUCTS
    => Facebook Login in developer facebook site.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,16 +4,23 @@ import { NavController } from 'ionic-angular';
 //imported
 import { LoginPage } from '../login/login';
 import { RegisterPage } from '../register/register';
+import { DashboardPage } from './../dashboard/dashboard';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+// import { firebase } from 'firebase';
+import * as firebase from 'firebase/app';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
+  constructor(
+    public navCtrl: NavController,
+    private fire_auth: AngularFireAuth
+  ) {}
 
   signIn() {
     this.navCtrl.push(LoginPage);
@@ -21,4 +28,18 @@ export class HomePage {
   signUp() {
     this.navCtrl.push(RegisterPage);
   }
+
+  loginWithFacebook() {
+    this.fire_auth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => {
+        console.log(res);
+        this.navCtrl.setRoot(DashboardPage);
+      });
+  }
+
+  logoutOfFacebook() {
+     this.fire_auth.auth.signOut();
+  }
+
 }
